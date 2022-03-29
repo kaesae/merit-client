4df4cf47402d057ce4ce7ae3e4456e36fef19127
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-03-29 11:01:33 -0700

    Sign In added, token usestate added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./Components/Footer/Footer";
 
 function App() {
   const [user, setUser] = useState({ email: "", password: "" });
+  const [token, setToken] = useState("")
 
 
   const handleSubmit = (event) => {
@@ -16,7 +17,7 @@ function App() {
       },
       method: "POST",
       body: JSON.stringify(user),
-    })
+    });
   };
 
   const handleChange = (event) => {
@@ -25,10 +26,36 @@ function App() {
       const editedUser = {
         ...prevUser,
         [event.target.name]: event.target.value,
-      }
+      };
       return editedUser;
+    });
+  };
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    fetch("http://127.0.0.1:8000/sign-in/", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(user),
     })
-  }
+    .then((response) => response.json())
+    .then((data) => setToken(data.user.token))
+  };
+
+  const handleSignOut = (event) => {
+    event.preventDefault();
+    fetch("http://127.0.0.1:8000/sign-out/", {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorizations": `Token ${token}`
+      },
+      method: "DELETE",
+    })
+    .then(() => console.log('You are signed out!'))
+    .then(() => setToken(""))
+  };
 
   return (
     <div className="App">
@@ -50,8 +77,22 @@ function App() {
           </form>
         </div>
 
+        <div className="sign-in">
+          <h2>Sign In!</h2>
+          <form onSubmit={handleSignIn}>
+            <label>
+              Email:
+              <input type="email" name="email" onChange={handleChange} />
+            </label>
+            <label>
+              Password:
+              <input type="password" name="password" onChange={handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
 
-
+        <button onClick={handleSignOut}>Sign Out</button>
       </main>
       <Footer />
     </div>
